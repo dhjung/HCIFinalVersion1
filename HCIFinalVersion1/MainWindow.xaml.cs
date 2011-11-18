ec91822c01ef290e2c7c813844a9671b72b5fdc6
dhjung <dhjung0201@gmail.com>
2011-11-17 20:03:13 -0800

    Initail Commit

---
+++ HCIFinalVersion1/MainWindow.xaml.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections;
+using Microsoft.Research.Kinect.Nui;
+using Coding4Fun.Kinect.Wpf;
+using Coding4Fun.Kinect.Wpf.Controls;
+using System.Diagnostics;
+using System.Windows.Media.Animation;
+
+namespace HCIFinalVersion1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private double WaitTime = 1500;
+        private static Stopwatch hoverWatch;
+        private static bool hoverTrigger = false;
+        private static Button currentButton = new Button();
+        private static bool popupWindow = false;
+
+        private const float ClickThreshold = 0.33f;
+        private const float SkeletonMaxX = 0.50f;
+        private const float SkeletonMaxY = 0.50f;
+            
+        //Declare the Kinect NUI Runtime instance
+        Microsoft.Research.Kinect.Nui.Runtime nui;
+        private static double _topBoundary;
+        private static double _bottomBoundary;
+        private static double _leftBoundary;
+        private static double _rightBoundary;
+        private static double _itemLeft;
+        private static double _itemTop;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            hoverWatch = new Stopwatch();
+
+            if (Runtime.Kinects.Count == 0)
+            {
+                Console.Write("missing kinect");
+            }
+            else
+            {
+                nui = Runtime.Kinects[0];
+                nui.Initialize(RuntimeOptions.UseColor | RuntimeOptions.UseDepth | RuntimeOptions.UseDepthAndPlayerIndex | RuntimeOptions.UseSkeletalTracking);
+
+                nui.VideoFrameReady += new EventHandler<ImageFrameReadyEventArgs>(nui_VideoFrameReady);
+                nui.DepthFrameReady += new EventHandler<ImageFrameReadyEventArgs>(nui_DepthFrameReady);
+
+                nui.VideoStream.Open(ImageStreamType.Video, 2, ImageResolution.Resolution640x480, ImageType.Color); //PoolSize = 2 buffers.  One for queuing and one for displaying
+                nui.DepthStream.Open(ImageStreamType.Depth, 2, ImageResolution.Resolution320x240, ImageType.Depth);
+
+                #region TransformSmooth
+                //Must set to true and set after call to Initialize
+                nui.SkeletonEngine.TransformSmooth = true;
+
+                //Use to transform and reduce jitter
+                var parameters = new TransformSmoothParameters
+                {
+                    Smoothing = 0.75f,
+                    Correction = 0.0f,
+                    Prediction = 0.0f,
+                    JitterRadius = 0.05f,
+                    MaxDeviationRadius = 0.04f
+                };
+
+                nui.SkeletonEngine.SmoothParameters = parameters;
+                #endregion
+
+                //add event to receive skeleton data
+                nui.SkeletonFrameReady += new EventHandler<SkeletonFrameReadyEventArgs>(nui_SkeletonFrameReady);
+            }
+
+        }
+
+        void nui_VideoFrameReady(object sender, ImageFrameReadyEventArgs e)
+        {
+
+            //PlanarImage imageData = e.ImageFrame.Image;
+            //image1.Source = BitmapSource.Create(imageData.Width, imageData.Height, 96, 96,
+            //                            PixelFormats.Bgr32, null, imageData.Bits, imageData.Width * imageData.BytesPerPixel);
+        }
+
+        void nui_DepthFrameReady(object sender, ImageFrameReadyEventArgs e)
+        {
+            //image2.Source = e.ImageFrame.ToBitmapSource();
+        }
+
+        void nui_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            SkeletonFrame allSkeletons = e.SkeletonFrame;
+
+            if (allSkeletons != null) 
+            {
+                //get the first tracked skeleton
+                SkeletonData skeleton = (from s in allSkeletons.Skeletons
+                                         where s.TrackingState == SkeletonTrackingState.Tracked
+                                         select s).FirstOrDefault();
+                if (skeleton != null)
+                {
+                    //Control the Mouse coordinates
+                    Joint jointRight = skeleton.Joints[JointID.HandRight];
+                    Joint scaledRight = jointRight.ScaleTo((int)SystemParameters.PrimaryScreenWidth, (int)SystemParameters.PrimaryScreenHeight, SkeletonMaxX, SkeletonMaxY);
+                    int cursorX, cursorY;
+                    cursorX = (int)scaledRight.Position.X;
+                    cursorY = (int)scaledRight.Position.Y;
+
+                    Joint jointLeft = skeleton.Joints[JointID.HandLeft];
+                    Joint scaledLeft = jointRight.ScaleTo((int)SystemParameters.PrimaryScreenWidth, (int)SystemParameters.PrimaryScreenHeight, SkeletonMaxX, SkeletonMaxY);
+                    int leftCursorX, leftCursorY;
+                    leftCursorX = (int)scaledRight.Position.X;
+                    leftCursorY = (int)scaledRight.Position.Y;
+
+                    //set positions
+                    SetEllipsePosition(headEllipse, skeleton.Joints[JointID.Head]);
+                    //SetEllipsePosition(handEllipse, skeleton.Joints[JointID.HandRight]);
+
+                    SetEllipsePosition(rightHandEllipse, skeleton.Joints[JointID.HandRight]);
+
+                    SetEllipsePosition(leftHandEllipse, skeleton.Joints[JointID.HandLeft]);
+
+                    //Check if the right hand is hovering over the following buttons
+                    CheckButton(One, rightHandEllipse, leftHandEllipse);
+                    CheckButton(Two, rightHandEllipse, leftHandEllipse);
+                    CheckButton(Three, rightHandEllipse, leftHandEllipse);
+                    CheckButton(Four, rightHandEllipse, leftHandEllipse);
+                    CheckButton(LoadImage, rightHandEllipse, leftHandEllipse);
+                    CheckButton(Six, rightHandEllipse, leftHandEllipse);
+                    CheckButton(Seven, rightHandEllipse, leftHandEllipse);
+                    CheckButton(Eight, rightHandEllipse, leftHandEllipse);
+
+                    //bool leftClick = false;
+                    //NativeMethods.SendMouseInput(cursorX, cursorY, (int)SystemParameters.PrimaryScreenWidth, (int)SystemParameters.PrimaryScreenHeight, leftClick);
+                    
+                }
+            }
+        }
+
+        private void SetEllipsePosition(FrameworkElement ellipse, Joint joint)
+        {
+            var scaledJoint = joint.ScaleTo(1230, 750, .5f, .5f);
+
+            if (joint.ID == JointID.HandRight)
+            {
+                Canvas.SetLeft(ellipse, scaledJoint.Position.X);
+                Canvas.SetTop(ellipse, scaledJoint.Position.Y);
+
+                if (currentButton.Name == "LoadImage" && hoverTrigger == true)
+                {
+                    if (hoverWatch.ElapsedMilliseconds > WaitTime)
+                    {
+                        OpenFileDialog();
+                        hoverTrigger = false;
+                        popupWindow = true;
+                    }
+                    //Console.WriteLine(hoverWatch.ElapsedMilliseconds);
+                }
+                //Console.WriteLine("right hand");
+            }
+            else if (joint.ID == JointID.HandLeft)
+            {
+                Canvas.SetLeft(ellipse, scaledJoint.Position.X);
+                Canvas.SetTop(ellipse, scaledJoint.Position.Y);
+            }
+            else if (joint.ID == JointID.Head)
+            {
+                Canvas.SetLeft(ellipse, scaledJoint.Position.X);
+                Canvas.SetTop(ellipse, scaledJoint.Position.Y);
+            }
+        }
+
+        /*
+        private void SetButtonPosition(FrameworkElement button, Joint joint)
+        {
+            var scaledJoint = joint.ScaleTo(1230, 750, .5f, .5f);
+            if (joint.ID == JointID.HandRight)
+            {
+                Canvas.SetLeft(button, scaledJoint.Position.X);
+                Canvas.SetTop(button, scaledJoint.Position.Y);
+            }
+        }
+        
+        void kinectButton_Clicked(object sender, RoutedEventArgs e)
+        {
+            TextBlock textBlock = new TextBlock();
+            textBlock.Text = "Triggered";
+            textBlock.FontSize = 42;
+            grid1.Children.Add(textBlock);
+        }
+         */
+
+        private static void CheckButton(Button button, Ellipse left_thumbStick, Ellipse right_thumbStick)
+        {
+            if ( (IsItemMidpointInContainer(button, left_thumbStick) || IsItemMidpointInContainer(button, right_thumbStick)) && button.Visibility == Visibility.Visible)
+            {
+                button.Background = Brushes.Green;
+                if (button.Name == "LoadImage")
+                {
+                    if (button == currentButton)
+                    {
+                        if (hoverTrigger == false)
+                        {
+                            hoverWatch.Reset();
+                            hoverWatch.Start();
+                        } //else continue counting time
+                    }
+                    else
+                    {
+                        currentButton = button;
+                        //cursor has moved from one button to another.  Reset stop watch
+                        hoverWatch.Reset();
+                        hoverWatch.Start();
+                    }
+                    hoverTrigger = true;
+                }
+            }
+            else
+            {
+                button.Background = Brushes.White;
+                if (button.Name == "LoadImage")
+                {
+                    hoverTrigger = false;
+                    hoverWatch.Reset();
+                }
+            }
+        }
+
+        public static bool IsItemMidpointInContainer(FrameworkElement container, FrameworkElement target)
+        {
+            FindValues(container, target);
+
+            if (_itemTop < _topBoundary || _bottomBoundary < _itemTop)
+            {
+                //Midpoint of target is outside of top or bottom
+                return false;
+            }
+
+            if (_itemLeft < _leftBoundary || _rightBoundary < _itemLeft)
+            {
+                //Midpoint of target is outside of left or right
+                return false;
+            }
+
+            return true;
+        }
+
+        private static void FindValues(FrameworkElement container, FrameworkElement target)
+        {
+            var containerTopLeft = container.PointToScreen(new System.Windows.Point());
+            var itemTopLeft = target.PointToScreen(new System.Windows.Point());
+
+            _topBoundary = containerTopLeft.Y;
+            _bottomBoundary = _topBoundary + container.ActualHeight;
+            _leftBoundary = containerTopLeft.X;
+            _rightBoundary = _leftBoundary + container.ActualWidth;
+
+            //use midpoint of item (width or height divided by 2)
+            _itemLeft = itemTopLeft.X + (target.ActualWidth / 2);
+            _itemTop = itemTopLeft.Y + (target.ActualHeight / 2);
+        }
+
+        #region Open File Dialog and Add Image on Canvas
+
+        private void OpenFileDialog()
+        {
+            // Configure open file dialog box
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            dlg.InitialDirectory = @"C:\Users\Public\Pictures\Sample Pictures\";
+
+            // Show open file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // Open document
+                string filename = dlg.FileName;
+
+                // Add Image on Canvas
+                AddImageOnCanvas(filename, new Point(0, 0));
+            }
+        }
+
+        private void AddImageOnCanvas(String path, Point pt)
+        {
+            //Create an new image
+            BitmapImage bitmap = new BitmapImage(new Uri(path));
+            Image image = new Image { Source = bitmap };
+
+            image.Height = bitmap.PixelHeight;
+            image.Width = bitmap.PixelWidth;
+
+            InkCanvas.SetLeft(image, pt.X);
+            InkCanvas.SetTop(image, pt.Y);
+            inkCanvas.Children.Add(image);
+
+            AdornerLayer adornerlayer = AdornerLayer.GetAdornerLayer(image);
+            MyImageAdorner adorner = new MyImageAdorner(image);
+            adornerlayer.Add(adorner);
+
+            inkCanvas.EditingMode = InkCanvasEditingMode.None;
+        }
+
+        #endregion
+    }
+}
---
+++ HCIFinalVersion1/MyImageAdorner.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
+
+namespace HCIFinalVersion1
+{
+    public class MyImageAdorner : Adorner
+    {
+        Thumb rotateHandle;
+        Thumb moveHandle;
+        Thumb scaleHandle;
+
+        Path outline;
+        VisualCollection visualChildren;
+        Point center;
+
+        TranslateTransform translate;
+        ScaleTransform scale;
+        RotateTransform rotation;
+        TransformGroup transformGroup;
+
+        const int HANDLEMARGIN = 10;
+
+        public MyImageAdorner(UIElement adornedElement)
+            : base(adornedElement)
+        {
+            visualChildren = new VisualCollection(this);
+
+            rotateHandle = new Thumb();
+            rotateHandle.Cursor = Cursors.SizeWE;
+            rotateHandle.Width = 20;
+            rotateHandle.Height = 20;
+            rotateHandle.Background = Brushes.Blue;
+
+            rotateHandle.DragDelta += new DragDeltaEventHandler(rotateHandle_DragDelta);
+            rotateHandle.DragCompleted += new DragCompletedEventHandler(rotateHandle_DragCompleted);
+
+            moveHandle = new Thumb();
+            moveHandle.Cursor = Cursors.SizeAll;
+            moveHandle.Width = 20;
+            moveHandle.Height = 20;
+            moveHandle.Background = Brushes.Yellow;
+
+            moveHandle.DragDelta += new DragDeltaEventHandler(moveHandle_DragDelta);
+            moveHandle.DragCompleted += new DragCompletedEventHandler(moveHandle_DragCompleted);
+
+            scaleHandle = new Thumb();
+            scaleHandle.Cursor = Cursors.SizeNWSE;
+            scaleHandle.Width = 20;
+            scaleHandle.Height = 20;
+            scaleHandle.Background = Brushes.Red;
+
+            scaleHandle.DragDelta += new DragDeltaEventHandler(scaleHandle_DragDelta);
+            scaleHandle.DragCompleted += new DragCompletedEventHandler(scaleHandle_DragCompleted);
+
+            outline = new Path();
+            outline.Stroke = Brushes.Gray;
+            outline.StrokeThickness = 10;
+
+            visualChildren.Add(outline);
+            visualChildren.Add(rotateHandle);
+            visualChildren.Add(moveHandle);
+            visualChildren.Add(scaleHandle);
+
+            rotation = new RotateTransform();
+            translate = new TranslateTransform();
+            scale = new ScaleTransform();
+            transformGroup = new TransformGroup();
+        }
+
+        void scaleHandle_DragCompleted(object sender, DragCompletedEventArgs e)
+        {
+            MoveNewTransformToAdornedElement(scale);
+        }
+
+        void scaleHandle_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            Point pos = Mouse.GetPosition(this);
+
+            // TODO: Add scale logic
+            scale.ScaleX = 0.5;
+            scale.ScaleY = 0.5;
+
+            scale.CenterX = center.X;
+            scale.CenterY = center.Y;
+
+            outline.RenderTransform = scale;
+        }
+
+        void moveHandle_DragCompleted(object sender, DragCompletedEventArgs e)
+        {
+            MoveNewTransformToAdornedElement(translate);
+        }
+
+        void moveHandle_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            Point pos = Mouse.GetPosition(this);
+
+            double deltaX = pos.X - center.X;
+            double deltaY = pos.Y - center.Y;
+
+            translate.X = deltaX;
+            translate.Y = deltaY;
+
+            outline.RenderTransform = translate;
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+
+            center = new Point(AdornedElement.RenderSize.Width / 2, AdornedElement.RenderSize.Height / 2);
+            Point bottomRight = new Point(AdornedElement.RenderSize.Width, AdornedElement.RenderSize.Height);
+
+            Rect rotateRect = new Rect(0,
+                                       0 - (AdornedElement.RenderSize.Height / 2 + HANDLEMARGIN),
+                                       AdornedElement.RenderSize.Width,
+                                       AdornedElement.RenderSize.Height);
+            Rect scaleRectUL = new Rect(0, 0, 10, 10);
+            Rect scaleRectBR = new Rect(bottomRight.X, bottomRight.Y, 10, 10);
+
+            Rect finalRect = new Rect(finalSize);
+
+
+            rotateHandle.Arrange(rotateRect);
+            moveHandle.Arrange(finalRect);
+            scaleHandle.Arrange(scaleRectUL);
+            scaleHandle.Arrange(scaleRectBR);
+
+            outline.Data = new RectangleGeometry(finalRect);
+            outline.Arrange(finalRect);
+
+            return finalSize;
+        }
+
+        void rotateHandle_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            Point pos = Mouse.GetPosition(this);
+
+            double deltaX = pos.X - center.X;
+            double deltaY = pos.Y - center.Y;
+
+            double angle;
+
+            if (deltaY.Equals(0))
+            {
+                if (!deltaX.Equals(0))
+                {
+                    angle = 90;
+                }
+                else
+                {
+                    return;
+                }
+            }
+            else
+            {
+                double tan = deltaX / deltaY;
+                angle = Math.Atan(tan);
+
+                angle = angle * 180 / Math.PI;
+            }
+
+            // If the mouse crosses the vertical center, 
+            // find the complementary angle.
+            if (deltaY > 0)
+            {
+                angle = 180 - Math.Abs(angle);
+            }
+
+            // Rotate left if the mouse moves left and right
+            // if the mouse moves right.
+            if (deltaX < 0)
+            {
+                angle = -Math.Abs(angle);
+            }
+            else
+            {
+                angle = Math.Abs(angle);
+            }
+
+            if (Double.IsNaN(angle))
+            {
+                return;
+            }
+
+            // Apply the rotation to the outline.
+            rotation.Angle = angle;
+            rotation.CenterX = center.X;
+            rotation.CenterY = center.Y;
+
+            outline.RenderTransform = rotation;
+        }
+
+        /// <summary>
+        /// Rotates to the same angle as outline.
+        /// </summary>
+        void rotateHandle_DragCompleted(object sender, DragCompletedEventArgs e)
+        {
+            MoveNewTransformToAdornedElement(rotation);
+        }
+
+        protected override int VisualChildrenCount
+        {
+            get { return visualChildren.Count; }
+        }
+
+        protected override Visual GetVisualChild(int index)
+        {
+            return visualChildren[index];
+        }
+
+        private void MoveNewTransformToAdornedElement(Transform transform)
+        {
+            if (transform == null)
+            {
+                return;
+            }
+            var newTransform = transform.Clone();
+            newTransform.Freeze();
+
+            transformGroup.Children.Insert(0, newTransform);
+
+            AdornedElement.RenderTransform = transformGroup;
+
+            outline.RenderTransform = Transform.Identity;
+            this.InvalidateArrange();
+        }
+
+    }
+}
